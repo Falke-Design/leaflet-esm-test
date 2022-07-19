9b6f614ece988896eea8594bda8e86b648c542d2
Jon Koops <jonkoops@gmail.com>
2022-07-19 10:28:04 +0200

    Update example code

--- js/index.js
+++ js/index.js
@@ -1,2 +1,15 @@
-//import './index-require';
-import './index-import';
+import L, { Map } from 'leaflet';
+import '@geoman-io/leaflet-geoman-free';
+
+const map = new Map('map', {
+  center: [0,0],
+  zoom: 1
+});
+
+console.log('import L', L);
+console.log('window.L', window.L);
+console.log('import L == window.L', L === window.L);
+
+console.log('import L.PM', L.PM);
+console.log('window.L.PM', window.L.PM);
+console.log('import L.PM === window.L.PM', L.PM === window.L.PM);
